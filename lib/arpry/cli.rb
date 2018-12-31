4aecf25dcdce15cad460aee8dffd0ffdac163c1d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-12-31 20:09:04 +0900

    Support belongs_to, has_many, and has_many through
    Fix #2

--- lib/arpry/cli.rb
+++ lib/arpry/cli.rb
@@ -30,10 +30,47 @@ module Arpry
     end
 
     def generate_classes
-      ApplicationRecord.connection.tables.each do |table|
+      classes = ApplicationRecord.connection.tables.map do |table|
         Namespace.const_set(table.classify, Class.new(ApplicationRecord) do
           self.table_name = table
         end)
+      end
+
+      relations = Array.new(classes.size) do
+        Array.new(classes.size)
+      end
+
+      classes.each.with_index do |klass, idx|
+        klass.columns.each do |col|
+          ref_name = col.name[/\A(.+)(_id|ID)\z/, 1]
+          next unless ref_name
+          ref_klass_idx = classes.find_index {|c| c.table_name == ref_name.singularize || c.table_name == ref_name.pluralize}
+          next unless ref_klass_idx
+
+          relations[idx][ref_klass_idx] = col.name
+        end
+      end
+
+      relations.each.with_index do |row, idx|
+        klass = classes[idx]
+        exists = row.map.with_index.select {|fk, _idx| fk}
+
+        exists.each do |fk, ref_klass_idx|
+          next unless fk
+
+          ref_klass = classes[ref_klass_idx]
+
+          klass.belongs_to ref_klass.table_name.singularize.to_sym, foreign_key: fk
+          ref_klass.has_many klass.table_name.pluralize.to_sym, foreign_key: fk
+        end
+
+        # from -> klass <- to
+        exists.permutation(2).each do |from, to|
+          from_klass = classes[from[1]]
+          to_fk = to[0]
+          to_klass = classes[to[1]]
+          from_klass.has_many to_klass.table_name.pluralize.to_sym, foreign_key: to_fk, through: klass.table_name.pluralize.to_sym
+        end
       end
     end
   end
